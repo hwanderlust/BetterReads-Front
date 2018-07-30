c561b4135f0c074e02ba96fc25b642df83d3d616
pablopawpaw <lpault29@gmail.com>
2018-07-30 17:49:17 -0400

    6books to 5books

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,7 +4,7 @@ import Book from './Book'
 const Books = ({ bestSellers, renderBook }) => {
   console.log(bestSellers);
   let bookNum = 0
-  const renderBooks = bestSellers.slice().splice(0,6).map(book => {
+  const renderBooks = bestSellers.slice().splice(0,5).map(book => {
     ++bookNum
     return <Book key={book.list_id} bookNo={`book-${bookNum}`} book={book} renderBook={renderBook} />
   })
