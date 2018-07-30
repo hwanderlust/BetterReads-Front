bc8f17e77de404393da5f742e4deb954eaea65e8
pablopawpaw <lpault29@gmail.com>
2018-07-30 17:40:48 -0400

    limit bestsellers to 6

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,9 +8,21 @@ const Book = ({ bookNo, book, renderBook }) => {
     renderBook(book)
   }
 
+  const hover = (e) => {
+    // e.target.style += `width: 15em; height: 15em;`
+    // e.target.ownerDocument.body.style.opacity += `0.5`
+  }
+
+  const stopHover = (e) => {
+    // e.target.style = `transform: scale(1, 1)`
+    // e.target.ownerDocument.body.style.opacity= `1`
+  }
+
   return (
     <div
       onClick={handleClick}
+      onMouseEnter={(e) => hover(e)}
+      onMouseLeave={(e) => stopHover(e)}
       className={bookNo}
       style={{
         background: 'url(' + book.books[0].book_image + ')',
@@ -18,7 +30,7 @@ const Book = ({ bookNo, book, renderBook }) => {
         backgroundRepeat: 'no-repeat'
       }}
     >
-      
+
     </div>
   );
 }
--- src/components/Books.js
+++ src/components/Books.js
@@ -3,9 +3,8 @@ import Book from './Book'
 
 const Books = ({ bestSellers, renderBook }) => {
   console.log(bestSellers);
-
   let bookNum = 0
-  const renderBooks = bestSellers.map(book => {
+  const renderBooks = bestSellers.slice().splice(0,6).map(book => {
     ++bookNum
     return <Book key={book.list_id} bookNo={`book-${bookNum}`} book={book} renderBook={renderBook} />
   })
